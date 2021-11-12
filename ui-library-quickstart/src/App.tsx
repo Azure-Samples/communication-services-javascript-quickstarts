3c5a4db7f752c2fb612f64ba000bab45fe0a4754
ddematheu2 <dademath@microsoft.com>
2021-11-11 17:57:49 -0800

    UI Library quickstart Readme

---
+++ ui-library-quickstart/src/App.tsx
@@ -0,0 +1,24 @@
+import { FluentThemeProvider } from '@azure/communication-react';
+import { Stack } from '@fluentui/react';
+import React from 'react';
+import { CallingComponents } from './CallingComponents';
+import { ChatComponents } from './ChatComponents';
+
+function CompletedComponentsApp(): JSX.Element {
+  const stackStyle = {
+    root: {
+      width: '100%'
+    }
+  };
+
+  return (
+    <FluentThemeProvider>
+      <Stack horizontal horizontalAlign="space-evenly" styles={stackStyle}>
+        <CallingComponents />
+        <ChatComponents />
+      </Stack>
+    </FluentThemeProvider>
+  );
+}
+
+export default CompletedComponentsApp;
---
+++ ui-library-starting-with-chat-stateful/src/ChatComponentsStateful.tsx
@@ -0,0 +1,21 @@
+import { usePropsFor, MessageThread, SendBox } from '@azure/communication-react';
+import React from 'react';
+
+function ChatComponents(): JSX.Element {
+  const messageThreadProps = usePropsFor(MessageThread);
+  const sendBoxProps = usePropsFor(SendBox);
+
+  return (
+    <>
+        <div style={{ height: '90vh', width: '100vw' }}>
+        {/*Props are updated asynchronously, so only render the component once props are populated.*/}
+        {messageThreadProps && <MessageThread {...messageThreadProps} />}
+        </div>
+        <div>
+        {sendBoxProps && <SendBox {...sendBoxProps} />}
+        </div>
+    </>
+  );
+}
+
+export default ChatComponents;
