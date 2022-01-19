cc9845b9781df96aeb5dda2cd70fdb283297b2b3
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-01-19 22:27:50 +0000

    lint and prettify code

--- ui-library-add-call-recording/app/src/App.tsx
+++ ui-library-add-call-recording/app/src/App.tsx
@@ -1,31 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        An app to demonstrate API calls.
-      </header>
+      <header className="App-header">An app to demonstrate API calls.</header>
       <DisplayApiResponse method="startRecording" />
       <DisplayApiResponse method="stopRecording" />
-
     </div>
   );
 }
 
 function DisplayApiResponse(props: { method: string }) {
   const { method } = props;
-  const [data, setData] = useState('NO RESPONSE YET');
+  const [data, setData] = useState("NO RESPONSE YET");
   useEffect(() => {
     (async function () {
-      const { text } = await (await fetch(`/api/${method}`, { method: 'POST', body: JSON.stringify({ 'callId': 'dummy' }) })).json();
-      console.log(method, text)
+      const { text } = await (
+        await fetch(`/api/${method}`, {
+          method: "POST",
+          body: JSON.stringify({ callId: "dummy" }),
+        })
+      ).json();
+      console.log(method, text);
       setData(text);
     })();
-  })
-  return <h2 className="App-response">Response from {method}: {data}</h2>
+  });
+  return (
+    <h2 className="App-response">
+      Response from {method}: {data}
+    </h2>
+  );
 }
 
 export default App;
