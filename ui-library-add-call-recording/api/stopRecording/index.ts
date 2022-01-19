e2b07c04eecba06df0cfd66a7621f70e1f6bbc7c
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-01-19 19:47:44 +0000

    Initialize Azure Functions

---
+++ ui-library-add-call-recording/api/stopRecording/index.ts
@@ -0,0 +1,21 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const callId = req.body?.callId;
+    context.log(`Processing startRecording(${callId})`);
+    if (!callId) {
+        context.res = {
+            // Bad request
+            status: 400,
+            body: 'callId is required'
+        }
+        return;
+    }
+
+    context.res = {
+        status: 200,
+        body: `Would have stopped recording for ${callId}`
+    };
+};
+
+export default httpTrigger;
