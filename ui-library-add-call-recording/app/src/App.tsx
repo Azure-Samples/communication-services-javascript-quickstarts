77840ae57da0b88add3848da70b7cee3cfcbe9dd
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-01-31 23:20:05 +0000

    Initialize UI library based frontend

--- ui-library-add-call-recording/app/src/App.tsx
+++ ui-library-add-call-recording/app/src/App.tsx
@@ -1,36 +1,65 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import { v1 } from 'uuid';
+import React, { useEffect, useMemo, useState } from 'react';
+import { AzureCommunicationTokenCredential, CommunicationUserIdentifier } from '@azure/communication-common';
+import {
+  CallComposite,
+  CallAdapter,
+  createAzureCommunicationCallAdapter,
+  fromFlatCommunicationIdentifier
+} from '@azure/communication-react';
+import { ACS_TOKEN, ACS_USER_ID } from './Secrets';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">An app to demonstrate API calls.</header>
-      <DisplayApiResponse method="startRecording" />
-      <DisplayApiResponse method="stopRecording" />
-    </div>
-  );
-}
 
-function DisplayApiResponse(props: { method: string }) {
-  const { method } = props;
-  const [data, setData] = useState("NO RESPONSE YET");
+function App(): JSX.Element {
+  const [callAdapter, setCallAdapter] = useState<CallAdapter>();
+
+  const userId = ACS_USER_ID;
+  if (!userId) {
+    throw new Error('No userId set');
+  }
+  const token = ACS_TOKEN;
+  if (!token) {
+    throw new Error('No token set');
+  }
+
+  const displayName = 'CallRecordingDemoUser';
+  const groupId = v1();
+
+  // We can't even initialize the adapter without a well-formed token.
+  const credential = useMemo(() => {
+    try {
+      return new AzureCommunicationTokenCredential(token);
+    } catch {
+      console.error('Failed to construct token credential');
+      return undefined;
+    }
+  }, [token]);
+
   useEffect(() => {
-    (async function () {
-      const { text } = await (
-        await fetch(`/api/${method}`, {
-          method: "POST",
-          body: JSON.stringify({ serverCallId: "dummy" }),
+    const createAdapter = async (): Promise<void> => {
+      setCallAdapter(
+        await createAzureCommunicationCallAdapter({
+          userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
+          displayName,
+          credential: new AzureCommunicationTokenCredential(token),
+          locator: { groupId }
         })
-      ).json();
-      console.log(method, text);
-      setData(text);
-    })();
-  });
-  return (
-    <h2 className="App-response">
-      Response from {method}: {data}
-    </h2>
-  );
+      );
+    };
+    createAdapter();
+  }, []);
+
+  if (!!callAdapter) {
+    return (
+      <div style={{ height: '100vh', display: 'flex' }}>
+        <CallComposite adapter={callAdapter} />
+      </div>
+    );
+  }
+  if (credential === undefined) {
+    return <h3>Failed to construct credential. Provided token is malformed.</h3>;
+  }
+  return <h3>Initializing...</h3>;
 }
 
 export default App;
---
+++ ui-library-add-call-recording/app/src/Secrets.tsx
@@ -0,0 +1,4 @@
+// Generate a user and token with the "Voice and video calling (VOIP)" scopes.
+// and paste the secrets here.
+export const ACS_USER_ID = "ENTER_USER_ID_HERE"
+export const ACS_TOKEN = "ENTER_TOKEN_HERE";
