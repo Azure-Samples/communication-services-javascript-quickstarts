e10458081249f61b329fc896b549e5e28adc1558
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-02-01 18:39:41 +0000

    Drop ACS references

--- ui-library-add-call-recording/app/src/Secrets.ts
+++ ui-library-add-call-recording/app/src/Secrets.ts
@@ -1,4 +1,4 @@
 // Generate a user and token with the "Voice and video calling (VOIP)" scopes.
 // and paste the secrets here.
-export const ACS_USER_ID = "INSERT_USER_ID"
-export const ACS_TOKEN = "INSERT_TOKEN"
+export const ACS_USER_ID = "REPLACE_WITH_USER_ID"
+export const ACS_TOKEN = "REPLACE_WITH_TOKEN"
