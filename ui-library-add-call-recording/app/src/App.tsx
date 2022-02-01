aa124796b95c57a516b2ffb1c0daad8cdd1b3024
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-02-01 00:16:38 +0000

    Partial button impl

--- ui-library-add-call-recording/app/src/App.tsx
+++ ui-library-add-call-recording/app/src/App.tsx
@@ -1,5 +1,5 @@
 import { v1 } from 'uuid';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { AzureCommunicationTokenCredential, CommunicationUserIdentifier } from '@azure/communication-common';
 import {
   CallComposite,
@@ -7,10 +7,16 @@ import {
   createAzureCommunicationCallAdapter,
   fromFlatCommunicationIdentifier,
   CustomCallControlButtonCallbackArgs,
-  CustomCallControlButtonProps
+  CustomCallControlButtonProps,
+  createStatefulCallClient,
+  createAzureCommunicationCallAdapterFromClient
 } from '@azure/communication-react';
 import { ACS_TOKEN, ACS_USER_ID } from './Secrets';
+import { initializeIcons } from '@fluentui/react';
+import { Record20Regular, RecordStop20Filled } from '@fluentui/react-icons';
 
+
+initializeIcons();
 
 function App(): JSX.Element {
   const [callAdapter, setCallAdapter] = useState<CallAdapter>();
@@ -37,19 +43,60 @@ function App(): JSX.Element {
     }
   }, [token]);
 
+  const [serverCallId, setServerCallId] = useState('');
+  const [recordingId, setRecordingId] = useState('');
+
   useEffect(() => {
     const createAdapter = async (): Promise<void> => {
-      setCallAdapter(
-        await createAzureCommunicationCallAdapter({
-          userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
-          displayName,
-          credential: new AzureCommunicationTokenCredential(token),
-          locator: { groupId }
-        })
-      );
+      const callClient = await createStatefulCallClient({
+        userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
+      })
+      const callAgent = await callClient.createCallAgent(new AzureCommunicationTokenCredential(token))
+      const newAdapter = await createAzureCommunicationCallAdapterFromClient(callClient, callAgent, { groupId });
+      setCallAdapter(newAdapter);
+      newAdapter.onStateChange(async (state) => {
+        if (state.call?.state === 'Connected') {
+          const call = callAgent.calls.find((call) => call.id === state.call?.id);
+          if (call) {
+            setServerCallId(await call.info.getServerCallId());
+          }
+        }
+      })
     };
     createAdapter();
   }, []);
+
+  const recordingButton = useCallback((args: CustomCallControlButtonCallbackArgs): CustomCallControlButtonProps => ({
+    placement: 'afterCameraButton',
+    showLabel: true,
+    labelKey: 'recordingButtonLabel',
+    strings: {
+      offLabel: "Start Recording",
+      onLabel: "Stop Recording",
+      tooltipOffContent: "Start Recording",
+      tooltipOnContent: "Stop Recording",
+    },
+    onRenderOffIcon: (): JSX.Element => (<Record20Regular />),
+    onRenderOnIcon: (): JSX.Element => (<RecordStop20Filled />),
+    checked: !!recordingId,
+    onClick: async () => {
+      if (!serverCallId) {
+        console.warn('Recording buton: No serverCallId yet!');
+        return;
+      }
+
+      if (!!recordingId) {
+        // stop the recording
+        await stopRecording(serverCallId, recordingId);
+        setRecordingId('');
+        return
+      }
+
+      // start the recording
+      const { recordingId: newRecordingId } = await startRecording(serverCallId);
+      setRecordingId(newRecordingId);
+    }
+  }), [serverCallId, recordingId]);
 
   const callCompositeOptions = useMemo(() => ({
     callControls: {
@@ -70,10 +117,26 @@ function App(): JSX.Element {
   return <h3>Initializing...</h3>;
 }
 
-const recordingButton = (args: CustomCallControlButtonCallbackArgs): CustomCallControlButtonProps => ({
-  placement: 'afterCameraButton',
-  showLabel: true,
-  labelKey: 'recordingButtonLabel',
+interface StartRecordingResponse {
+  recordingId: string;
+}
 
-})
+const startRecording = async (serverCallId: string): Promise<StartRecordingResponse> => {
+  const { recordingId } = await (
+    await fetch(`/api/startRecording`, {
+      method: "POST",
+      body: JSON.stringify({ serverCallId: serverCallId }),
+    })
+  ).json();
+  console.log(`Started recording for ${serverCallId}: ${recordingId}`);
+  return { recordingId };
+}
+
+const stopRecording = async (serverCallId: string, recordingId: string): Promise<void> => {
+  await fetch(`/api/stopRecording`, {
+    method: "POST",
+    body: JSON.stringify({ serverCallId: serverCallId }),
+  })
+  console.log(`Stopped recording for ${serverCallId}: ${recordingId}`);
+}
 export default App;
