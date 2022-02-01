5e3a3d2e0864795cbd7ccbcd629d717b752aa2fe
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-02-01 17:28:47 +0000

    Implement start/stop recording with app

---
+++ ui-library-add-call-recording/app/src/Secrets.ts
@@ -0,0 +1,4 @@
+// Generate a user and token with the "Voice and video calling (VOIP)" scopes.
+// and paste the secrets here.
+export const ACS_USER_ID = "INSERT_USER_ID"
+export const ACS_TOKEN = "INSERT_TOKEN"
