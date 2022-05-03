f52ecf6e98575e3384c1c5fdcdfe241c75283937
Anjul Garg <anjulgarg@live.com>
2022-05-03 18:58:57 +0000

    Removing secrets

--- ui-library-filesharing-ui-components/app/src/App.tsx
+++ ui-library-filesharing-ui-components/app/src/App.tsx
@@ -17,14 +17,13 @@ function App(): JSX.Element {
   registerIcons({ icons: DEFAULT_COMPONENT_ICONS });
   initializeFileTypeIcons();
 
-  const endpointUrl = "https://web-ui-dev.communication.azure.com";
-  const userAccessToken =
-    "eyJhbGciOiJSUzI1NiIsImtpZCI6IjEwNCIsIng1dCI6IlJDM0NPdTV6UENIWlVKaVBlclM0SUl4Szh3ZyIsInR5cCI6IkpXVCJ9.eyJza3lwZWlkIjoiYWNzOmRkOTc1M2MwLTZlNjItNGY3NC1hYjBmLWM5NGY5NzIzYjRlYl8wMDAwMDAxMS0wMWQxLTVmMjUtNTcwYy0xMTNhMGQwMDRkMGUiLCJzY3AiOjE3OTIsImNzaSI6IjE2NTA5MTA1NDIiLCJleHAiOjE2NTA5OTY5NDIsImFjc1Njb3BlIjoidm9pcCxjaGF0IiwicmVzb3VyY2VJZCI6ImRkOTc1M2MwLTZlNjItNGY3NC1hYjBmLWM5NGY5NzIzYjRlYiIsImlhdCI6MTY1MDkxMDU0Mn0.Ux1WzfWI_RIxgD7bSSnAjTq2wlGSIE5QrZH40O7vdIJzhvfNDGXJlhcqxUqO2jKfkFmTeOEq_eBEiDDcpR65ImP11ciZMqSZLdYwmf5cwPIu42Jj7JNYLalnYzL2CKMHNqaNK3S02PXJi1HBAYqPUbGv2MKa1b2cRgGD19Ev-hsfzAQL9hRPngLfIoAALZ8Hd6j-NLS8v3gLTrI7R8MVtS0EqSilkBK-drbFtzWohlhhTw7gwyp_zkfhI5n2vdfJB0iZUshCwqLCCammMja37JIjhJ8lQK5oXhI0UreokmytdFfORYTAORMt2WFfNxcpN15ksHtjj_9xpbsCbSu2pQ";
-  const userId = "8:acs:dd9753c0-6e62-4f74-ab0f-c94f9723b4eb_00000011-01d1-5f25-570c-113a0d004d0e";
+  const endpointUrl = "";
+  const userAccessToken = "";
+  const userId = "";
   const tokenCredential = new AzureCommunicationTokenCredential(
     userAccessToken
   );
-  const threadId = "19:XvuYShE3XIo8u10lzMES8_FdNYDoonp11NDEqhznuHc1@thread.v2";
+  const threadId = "";
   const displayName = "John Doe";
 
   // Instantiate the statefulChatClient
