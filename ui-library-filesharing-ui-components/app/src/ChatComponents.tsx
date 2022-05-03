b97c355171f8a10641b9359bb740673a452b3da1
Anjul Garg <anjulgarg@live.com>
2022-05-03 00:18:28 +0000

    partially working

--- ui-library-filesharing-ui-components/app/src/ChatComponents.tsx
+++ ui-library-filesharing-ui-components/app/src/ChatComponents.tsx
@@ -3,22 +3,91 @@ import {
   MessageThread,
   SendBox,
   ActiveFileUpload,
+  FileMetadata,
 } from "@azure/communication-react";
 import React from "react";
+import axios from "axios";
+import Form from "form-data";
+import { v4 } from "uuid";
 
 function ChatComponents(): JSX.Element {
   const messageThreadProps = usePropsFor(MessageThread);
   const sendBoxProps = usePropsFor(SendBox);
-
+  const allActiveFileUploads = React.useRef<ActiveFileUpload[] | []>([]);
+  const [files, setFiles] = React.useState<File[] | []>();
   const [activeFileUploads, setActiveFileUploads] = React.useState<
     ActiveFileUpload[] | []
-  >([
-    {
-      filename: "file1.txt",
-      id: "file1",
-      progress: 0.5,
-    },
-  ]);
+  >([]);
+  const completedFileUploads = React.useRef<FileMetadata[] | []>([]);
+
+  React.useEffect(() => {
+    const updateFileUploadProgress = (
+      fileId: string,
+      progress: number,
+      complete: boolean = false
+    ) => {
+      const updatedData = allActiveFileUploads.current.map((active) => {
+        if (active.id === fileId) {
+          return {
+            ...active,
+            progress,
+            uploadComplete: complete,
+          };
+        }
+        return active;
+      });
+      allActiveFileUploads.current = updatedData;
+      setActiveFileUploads(updatedData);
+    };
+
+    const uploadFile = async (file: File): Promise<void> => {
+      const extension = file.name.split(".").pop() || "";
+      const uniqueFileName = `${v4()}-${file.name}`;
+      const data = new Form();
+      data.append("file", file);
+
+      axios
+        .request({
+          method: "post",
+          url: `/api/UploadFileToAzureBlobStore?filename=${uniqueFileName}`,
+          data: data,
+          onUploadProgress: (p) => {
+            updateFileUploadProgress(file.name, p.loaded / p.total);
+          },
+        })
+        .then((res) => {
+          updateFileUploadProgress(file.name, 1, true);
+          completedFileUploads.current = [
+            ...completedFileUploads.current,
+            {
+              name: file.name,
+              extension,
+              url: res.data.url,
+            },
+          ];
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    if (files?.length) {
+      files.forEach((file) => {
+        uploadFile(file);
+      });
+    }
+  }, [files]);
+
+  const onChange = (files: FileList | null) => {
+    if (!files) return;
+    setFiles(Array.from(files));
+    allActiveFileUploads.current = Array.from(files).map((file) => ({
+      filename: file.name,
+      id: file.name,
+      progress: 0,
+    }));
+    setActiveFileUploads(allActiveFileUploads.current);
+  };
 
   return (
     <div style={{ height: "100%", width: "100%" }}>
@@ -29,25 +98,35 @@ function ChatComponents(): JSX.Element {
           {...sendBoxProps}
           activeFileUploads={activeFileUploads}
           onCancelFileUpload={(id: string) => {
-            setActiveFileUploads(
-              activeFileUploads.filter((upload) => upload.id !== id)
+            allActiveFileUploads.current = allActiveFileUploads.current.filter(
+              (upload) => upload.id !== id
             );
+            setActiveFileUploads(allActiveFileUploads.current);
           }}
           onSendMessage={async (message: string) => {
             sendBoxProps.onSendMessage(message, {
               metadata: {
-                fileSharingMetadata: JSON.stringify([
-                  {
-                    name: "TestFile.pdf",
-                    extension: "pdf",
-                    url: "https://test.com/test.pdf",
-                  },
-                ]),
+                fileSharingMetadata: JSON.stringify(completedFileUploads.current),
               },
             });
+
+            allActiveFileUploads.current = [];
+            completedFileUploads.current = [];
+            setActiveFileUploads([]);
           }}
         />
       )}
+
+      <div style={{ padding: "0.25rem" }}>
+        <input
+          name="file"
+          type="file"
+          multiple
+          onChange={(event) => {
+            onChange(event.currentTarget.files);
+          }}
+        />
+      </div>
     </div>
   );
 }
