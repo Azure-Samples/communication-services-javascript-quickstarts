ef14b50dc4c35ef5cf93b1ab2e4501121fbbe4e7
Anjul Garg <anjulgarg@live.com>
2022-05-26 20:20:28 +0000

    Resolving ARB Comments. Installing prettier as dev dependency. Commenting code. Converted try catch to await.

--- ui-library-filesharing-chat-composite/api/UploadFileToAzureBlobStore/index.ts
+++ ui-library-filesharing-chat-composite/api/UploadFileToAzureBlobStore/index.ts
@@ -1,24 +1,30 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import { AzureFunction, Context, HttpRequest } from '@azure/functions';
-import * as multipart from 'parse-multipart';
-import HTTP_CODES from 'http-status-enum';
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import * as multipart from "parse-multipart";
+import HTTP_CODES from "http-status-enum";
 
-const azureStorageConnectionString = process.env['azureStorageConnectionString'];
-let storageAccountName;
+const azureStorageConnectionString =
+  process.env["azureStorageConnectionString"];
+
+let storageAccountName: string | undefined;
+
 try {
-  storageAccountName = azureStorageConnectionString.match(/AccountName=([^;]+)/)[1];
+  storageAccountName =
+    azureStorageConnectionString.match(/AccountName=([^;]+)/)[1];
 } catch {
-  console.error('Failed to parse Azure storage name from connection string');
+  throw new Error("Invalid Azure Storage Connection String");
 }
 
 // Name of the container in Azure Storage to store all files in.
 // Make sure that this name matches the value in the 'function.json' file.
-const fileSharingUploadsContainerName = 'uploads';
+const fileSharingUploadsContainerName = "uploads";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<unknown> {
-  context.log('UploadFileToAzureBlobStore: Processing new request.');
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<unknown> {
+  context.log("UploadFileToAzureBlobStore: Processing new request.");
 
   if (!req.body || !req.body.length) {
     context.res.body = { error: `Request body is not defined` };
@@ -34,53 +40,58 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
   }
 
   // Content type is required to know how to parse multi-part form
-  if (!req.headers || !req.headers['content-type']) {
-    context.res.body = { error: `Content type is not sent in header 'content-type'` };
+  if (!req.headers || !req.headers["content-type"]) {
+    context.res.body = {
+      error: `Content type is not sent in header 'content-type'`,
+    };
     context.res.status = HTTP_CODES.BAD_REQUEST;
     return;
   }
 
   context.log(
-    `*** Uploading Filename:${req.query?.filename}, Content type:${req.headers['content-type']}, Length:${req.body.length}`
+    `*** Uploading Filename:${req.query?.filename}, Content type:${req.headers["content-type"]}, Length:${req.body.length}`
   );
 
   if (!azureStorageConnectionString) {
-    throw Error("Storage isn't configured correctly - connection string missing in `azureStorageConnectionString`");
+    throw Error(
+      "Storage isn't configured correctly - connection string missing in `azureStorageConnectionString`"
+    );
   }
 
   try {
     // Each chunk of the file is delimited by a special string
     const bodyBuffer = Buffer.from(req.body);
-    const boundary = multipart.getBoundary(req.headers['content-type']);
+    const boundary = multipart.getBoundary(req.headers["content-type"]);
     const parts = multipart.Parse(bodyBuffer, boundary);
 
     // The file buffer is corrupted or incomplete
     if (!parts?.length) {
-      context.res.body = { error: `File buffer is incorrect`};
+      context.res.body = { error: `File buffer is incorrect` };
       context.res.status = HTTP_CODES.BAD_REQUEST;
       return;
     }
 
     // filename is a required property of the parse-multipart package
-    if (parts[0]?.filename) context.log(`Original filename = ${parts[0]?.filename}`);
+    if (parts[0]?.filename)
+      context.log(`Original filename = ${parts[0]?.filename}`);
     if (parts[0]?.type) context.log(`Content type = ${parts[0]?.type}`);
     if (parts[0]?.data?.length) context.log(`Size = ${parts[0]?.data?.length}`);
 
     // Any data assigned to this binding is uploaded to azure storage by the output variable binding.
     // only parts[0].data is accessed as only one file is uploaded per request, other parts are never populated.
     context.bindings.storage = parts[0]?.data;
-    
+
     context.res.body = {
       filename: req.query?.filename,
       storageAccountName,
       fileSharingUploadsContainerName,
-      url: `https://${storageAccountName}.blob.core.windows.net/${fileSharingUploadsContainerName}/${req.query?.filename}`
+      url: `https://${storageAccountName}.blob.core.windows.net/${fileSharingUploadsContainerName}/${req.query?.filename}`,
     };
     return;
   } catch (err) {
     context.log.error(err.message);
     context.res.body = {
-      error: err.message
+      error: err.message,
     };
     context.res.status = HTTP_CODES.INTERNAL_SERVER_ERROR;
     return;
