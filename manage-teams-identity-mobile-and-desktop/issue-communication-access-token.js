9104c672081e6c97baba97a59050abbf86d302d1
Aigerim Beishenbekova <aigerimb@microsoft.com>
2022-07-13 15:54:34 +0200

    added comments

--- manage-teams-identity-mobile-and-desktop/issue-communication-access-token.js
+++ manage-teams-identity-mobile-and-desktop/issue-communication-access-token.js
@@ -2,11 +2,13 @@ const { CommunicationIdentityClient } = require('@azure/communication-identity')
 const { PublicClientApplication, CryptoProvider } = require('@azure/msal-node');
 const express = require("express");
 
+// You will need to set environment variables and edit the following values
 const SERVER_PORT = process.env.PORT || 80;
 const REDIRECT_URI = `http://localhost:${SERVER_PORT}/redirect`;
-
 const clientId = "<contoso_application_id>";
 const tenantId = "<contoso_tenant_id>"; 
+
+// Create configuration object that will be passed to MSAL instance on creation.
 const msalConfig = {
     auth: {
         clientId: clientId,
@@ -14,6 +16,7 @@ const msalConfig = {
     }
 };
 
+// Create an instance of PublicClientApplication
 const pca = new PublicClientApplication(msalConfig);
 const provider = new CryptoProvider();
 
@@ -22,6 +25,7 @@ const app = express();
 let pkceVerifier = "";
 
 app.get('/', async (req, res) => {
+    // Generate PKCE Codes before starting the authorization flow
     const {verifier, challenge} = await provider.generatePkceCodes();
     pkceVerifier = verifier;
     
@@ -34,13 +38,14 @@ app.get('/', async (req, res) => {
         codeChallenge: challenge, 
         codeChallengeMethod: "S256"
     };
-
+    // Get url to sign user in and consent to scopes needed for application
     pca.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
         res.redirect(response);
     }).catch((error) => console.log(JSON.stringify(error)));
 });
 
 app.get('/redirect', async (req, res) => {
+    // Create request parameters object for acquiring the AAD token and object ID of a Teams user
     const tokenRequest = {
         code: req.query.code,
         scopes: [
@@ -51,7 +56,8 @@ app.get('/redirect', async (req, res) => {
         codeVerifier: pkceVerifier,
     };
 
-    pca.acquireTokenByCode(tokenRequest).then((response) => {
+    // Retrieve the AAD token and object ID of a Teams user
+    pca.acquireTokenByCode(tokenRequest).then(async(response) => {
         console.log("Response:", response);
         let teamsUserAadToken = response.accessToken;
         let userObjectId = response.uniqueId;
@@ -63,11 +69,12 @@ app.get('/redirect', async (req, res) => {
         // Instantiate the identity client
         const identityClient = new CommunicationIdentityClient(connectionString);
         
+        //Exchange the Azure AD access token of the Teams User for a Communication Identity access token
         let accessToken = await identityClient.getTokenForTeamsUser({
             teamsUserAadToken: teamsUserAadToken,
             clientId: clientId,
             userObjectId: userObjectId,
-        });;
+        });
         console.log("Token:", accessToken);
         
         res.sendStatus(200);
