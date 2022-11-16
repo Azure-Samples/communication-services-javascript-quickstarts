1540d0e9881cf4d73db413f69f2ee218571335e1
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-11-16 20:16:55 +0000

    initial project clone

---
+++ ui-library-call-readiness/src/App.tsx
@@ -0,0 +1,30 @@
+import { FluentThemeProvider } from '@azure/communication-react';
+import { initializeIcons, registerIcons, Stack } from '@fluentui/react';
+import React from 'react';
+import { CallingComponents } from './CallingComponents';
+import { ChatComponents } from './ChatComponents';
+import {
+  DEFAULT_COMPONENT_ICONS
+} from '@azure/communication-react';
+
+function CompletedComponentsApp(): JSX.Element {
+  const stackStyle = {
+    root: {
+      width: '100%'
+    }
+  };
+
+  initializeIcons();
+  registerIcons({ icons: DEFAULT_COMPONENT_ICONS });
+
+  return (
+    <FluentThemeProvider>
+      <Stack horizontal horizontalAlign="space-evenly" styles={stackStyle}>
+        <CallingComponents />
+        <ChatComponents />
+      </Stack>
+    </FluentThemeProvider>
+  );
+}
+
+export default CompletedComponentsApp;
