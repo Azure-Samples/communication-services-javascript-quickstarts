f4fecb97181b9e1d7d413079a15a8c044f2119ab
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2023-01-25 08:26:11 -0800

    Add basic parsing to the teams meeting URL to extract the chat thread ID automatically

--- join-chat-to-teams-meeting/client.js
+++ join-chat-to-teams-meeting/client.js
@@ -1,7 +1,7 @@
-import { CallClient, CallAgent } from "@azure/communication-calling";
-import { AzureCommunicationTokenCredential } from "@azure/communication-common";
-import { CommunicationIdentityClient } from "@azure/communication-identity";
-import { ChatClient } from "@azure/communication-chat";
+import { CallClient, CallAgent } from '@azure/communication-calling';
+import { AzureCommunicationTokenCredential } from '@azure/communication-common';
+import { CommunicationIdentityClient } from '@azure/communication-identity';
+import { ChatClient } from '@azure/communication-chat';
 
 let call;
 let callAgent;
@@ -10,125 +10,129 @@ let chatThreadClient;
 
 const meetingLinkInput = document.getElementById('teams-link-input');
 const threadIdInput = document.getElementById('thread-id-input');
-const callButton = document.getElementById("join-meeting-button");
-const hangUpButton = document.getElementById("hang-up-button");
+const callButton = document.getElementById('join-meeting-button');
+const hangUpButton = document.getElementById('hang-up-button');
 const callStateElement = document.getElementById('call-state');
 
-const messagesContainer = document.getElementById("messages-container");
-const chatBox = document.getElementById("chat-box");
-const sendMessageButton = document.getElementById("send-message");
-const messagebox = document.getElementById("message-box");
+const messagesContainer = document.getElementById('messages-container');
+const chatBox = document.getElementById('chat-box');
+const sendMessageButton = document.getElementById('send-message');
+const messageBox = document.getElementById('message-box');
 
 var userId = '';
 var messages = '';
 
 async function init() {
-	const connectionString = "<SECRET_CONNECTION_STRING>";
-	const endpointUrl = "<ENDPOINT_URL>";
+  const connectionString =
+    'endpoint=https://acs-ui-dev.communication.azure.com/;accesskey=imlXG4BniVo0S5d0bhPyq4sLT2x9CQN+KlWINIPNkhAFP2QjCXnQiCds6xUutrQ1eYKEVCpYbvjba/OirATWCg==';
+  const endpointUrl = 'https://acs-ui-dev.communication.azure.com';
 
-	const identityClient = new CommunicationIdentityClient(connectionString);
+  const identityClient = new CommunicationIdentityClient(connectionString);
 
-	let identityResponse = await identityClient.createUser();
-	userId = identityResponse.communicationUserId;
-	console.log(`\nCreated an identity with ID: ${identityResponse.communicationUserId}`);
+  let identityResponse = await identityClient.createUser();
+  userId = identityResponse.communicationUserId;
+  console.log(`\nCreated an identity with ID: ${identityResponse.communicationUserId}`);
 
-	let tokenResponse = await identityClient.getToken(identityResponse, [
-		"voip",
-		"chat",
-	]);
+  let tokenResponse = await identityClient.getToken(identityResponse, ['voip', 'chat']);
 
-	const { token, expiresOn } = tokenResponse;
-	console.log(`\nIssued an access token that expires at: ${expiresOn}`);
-	console.log(token);
+  const { token, expiresOn } = tokenResponse;
+  console.log(`\nIssued an access token that expires at: ${expiresOn}`);
+  console.log(token);
 
-	const callClient = new CallClient();
-	const tokenCredential = new AzureCommunicationTokenCredential(token);
-	callAgent = await callClient.createCallAgent(tokenCredential);
-	callButton.disabled = false;
+  const callClient = new CallClient();
+  const tokenCredential = new AzureCommunicationTokenCredential(token);
+  callAgent = await callClient.createCallAgent(tokenCredential);
+  callButton.disabled = false;
 
-	chatClient = new ChatClient(
-		endpointUrl,
-		new AzureCommunicationTokenCredential(token)
-	);
+  chatClient = new ChatClient(endpointUrl, new AzureCommunicationTokenCredential(token));
 
-	console.log('Azure Communication Chat client created!');
+  console.log('Azure Communication Chat client created!');
 }
 
 init();
 
-callButton.addEventListener("click", async () => {
-	// join with meeting link
-	call = callAgent.join({meetingLink: meetingLinkInput.value}, {});
+meetingLinkInput.addEventListener('input', () => {
+  try {
+    // Chat thread Id is included in the meeting link, usually the
+    // part containing the word 'thread'
+    const url = new URL(meetingLinkInput.value);
+    const decodedPath = decodeURIComponent(url.pathname);
+    threadIdInput.value = decodedPath.split('/').find((val) => val.includes('thread'));
+  } catch (e) {
+    // do nothing
+  }
+});
 
-	call.on('stateChanged', () => {
-	    callStateElement.innerText = call.state;
-	})
-	// toggle button and chat box states
-	chatBox.style.display = "block";
-	hangUpButton.disabled = false;
-	callButton.disabled = true;
+callButton.addEventListener('click', async () => {
+  // join with meeting link
+  call = callAgent.join({ meetingLink: meetingLinkInput.value }, {});
 
-	messagesContainer.innerHTML = messages;
+  call.on('stateChanged', () => {
+    callStateElement.innerText = call.state;
+  });
+  // toggle button and chat box states
+  chatBox.style.display = 'block';
+  hangUpButton.disabled = false;
+  callButton.disabled = true;
 
-	console.log(call);
+  messagesContainer.innerHTML = messages;
 
-	// open notifications channel
-	await chatClient.startRealtimeNotifications();
+  console.log(call);
 
-	// subscribe to new message notifications
-	chatClient.on("chatMessageReceived", (e) => {
-		console.log("Notification chatMessageReceived!");
+  // open notifications channel
+  await chatClient.startRealtimeNotifications();
 
-      // check whether the notification is intended for the current thread
-		if (threadIdInput.value != e.threadId) {
-			return;
-		}
+  // subscribe to new message notifications
+  chatClient.on('chatMessageReceived', (e) => {
+    console.log('Notification chatMessageReceived!');
 
-		if (e.sender.communicationUserId != userId) {
-		   renderReceivedMessage(e.message);
-		}
-		else {
-		   renderSentMessage(e.message);
-		}
-	});
+    // check whether the notification is intended for the current thread
+    if (threadIdInput.value != e.threadId) {
+      return;
+    }
 
-	chatThreadClient = await chatClient.getChatThreadClient(threadIdInput.value);
+    if (e.sender.communicationUserId != userId) {
+      renderReceivedMessage(e.message);
+    } else {
+      renderSentMessage(e.message);
+    }
+  });
+
+  chatThreadClient = await chatClient.getChatThreadClient(threadIdInput.value);
 });
 
 async function renderReceivedMessage(message) {
-	messages += '<div class="container lighter">' + message + '</div>';
-	messagesContainer.innerHTML = messages;
+  messages += '<div class="container lighter">' + message + '</div>';
+  messagesContainer.innerHTML = messages;
 }
 
 async function renderSentMessage(message) {
-	messages += '<div class="container darker">' + message + '</div>';
-	messagesContainer.innerHTML = messages;
+  messages += '<div class="container darker">' + message + '</div>';
+  messagesContainer.innerHTML = messages;
 }
 
-hangUpButton.addEventListener("click", async () => 
-	{
-		// end the current call
-		await call.hangUp();
+hangUpButton.addEventListener('click', async () => {
+  // end the current call
+  await call.hangUp();
 
-		// toggle button states
-		hangUpButton.disabled = true;
-		callButton.disabled = false;
-		callStateElement.innerText = '-';
+  // toggle button states
+  hangUpButton.disabled = true;
+  callButton.disabled = false;
+  callStateElement.innerText = '-';
 
-		// toggle chat states
-		chatBox.style.display = "none";
-		messages = "";
-	});
+  // toggle chat states
+  chatBox.style.display = 'none';
+  messages = '';
+});
 
-sendMessageButton.addEventListener("click", async () =>
-	{
-		let message = messagebox.value;
+sendMessageButton.addEventListener('click', async () => {
+  let message = messageBox.value;
 
-		let sendMessageRequest = { content: message };
-		let sendMessageOptions = { senderDisplayName : 'Jack' };
-		let sendChatMessageResult = await chatThreadClient.sendMessage(sendMessageRequest, sendMessageOptions);
-		let messageId = sendChatMessageResult.id;
+  let sendMessageRequest = { content: message };
+  let sendMessageOptions = { senderDisplayName: 'Jack' };
+  let sendChatMessageResult = await chatThreadClient.sendMessage(sendMessageRequest, sendMessageOptions);
+  let messageId = sendChatMessageResult.id;
 
-		messagebox.value = '';
-		console.log(`Message sent!, message id:${messageId}`);
-	});
+  messageBox.value = '';
+  console.log(`Message sent!, message id:${messageId}`);
+});
