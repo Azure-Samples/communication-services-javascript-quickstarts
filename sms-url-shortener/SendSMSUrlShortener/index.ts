5df3e8bbe1ef2a828598de4c6608c023a9ee5712
David de Matheu <dademath@microsoft.com>
2023-03-20 11:56:21 -0700

    Adding url shortener stuff

---
+++ sms-url-shortener/SendSMSUrlShortener/index.ts
@@ -0,0 +1,49 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { SmsClient }  from "@azure/communication-sms"
+
+// Initialize required variables
+const connectionString = "INSERT YOUR AZURE COMMUNICATION SERVICES CONNECTION STRING";
+const phoneNumber = "INSERT YOUR AZURE COMMUNICATION SERVICES PHONE NUMBER" // Ex. +15555555555
+const urlShortener = "INSERT YOUR URL SHORTENER API ENDPOINT" // Ex. https://<Azure Function URL>/api/UrlCreate
+
+// Instantiate the SMS client.
+const smsClient = new SmsClient(connectionString);
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    let to = req.query.phoneNumber; // Get phone number to send SMS to
+    let urlToShorten =  req.query.url; // Get URL to shorten
+    const body =  JSON.stringify({ "Url": urlToShorten})
+
+    await fetch(urlShortener, {
+      method: 'POST',
+      body: body
+    })
+    .then(res => res.json())
+    .then(async data => {
+      const url = data["ShortUrl"]
+      const sendResults = await smsClient.send({
+        from: phoneNumber,
+        to: [to],
+        message: "Join your scheduled appointment here: " + url
+      }, {
+        enableDeliveryReport: true
+      });
+
+      // Individual messages can encounter errors during sending.
+      // Use the "successful" property to verify the status.
+      for (const sendResult of sendResults) {
+        if (sendResult.successful) {
+          console.log("Success: ", sendResult);
+        } else {
+          console.error("Something went wrong when trying to send this message: ", sendResult);
+        }
+      }
+      context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: url
+      };
+    })
+
+};
+
+export default httpTrigger;
