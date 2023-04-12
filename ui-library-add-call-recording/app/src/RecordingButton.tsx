02e958ee8cec2a5efa1d42cefec7bb8d4f13a73a
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2023-04-12 11:00:35 -0700

    fix custom button fields

--- ui-library-add-call-recording/app/src/RecordingButton.tsx
+++ ui-library-add-call-recording/app/src/RecordingButton.tsx
@@ -1,28 +1,30 @@
 import { CustomCallControlButtonCallback, CustomCallControlButtonCallbackArgs } from "@azure/communication-react";
 import { Record20Regular, RecordStop20Filled } from "@fluentui/react-icons";
 import { startRecording, stopRecording } from "./Api";
+import { registerIcons } from "@fluentui/react";
+
+registerIcons({
+    icons: {
+        StartRecording: <Record20Regular />,
+        StopRecording: <RecordStop20Filled />
+    }
+});
 
 export const recordingButtonPropsCallback = (serverCallId: string, recordingId: string, setRecordingId: (recordingId: string) => void): CustomCallControlButtonCallback => {
+    const isRecording = !!recordingId;
     return (args: CustomCallControlButtonCallbackArgs) => ({
         placement: 'primary',
-        showLabel: args.displayType !== 'compact',
-        labelKey: 'recordingButtonLabel',
-        strings: {
-            offLabel: "Start Recording",
-            onLabel: "Stop Recording",
-            tooltipOffContent: "Start Recording",
-            tooltipOnContent: "Stop Recording",
-        },
-        onRenderOffIcon: (): JSX.Element => (<Record20Regular />),
-        onRenderOnIcon: (): JSX.Element => (<RecordStop20Filled />),
-        checked: !!recordingId,
-        onClick: async () => {
+        key: 'recordingButton',
+        showLabel: true,
+        text: isRecording ? 'Stop Recording' : 'Start Recording',
+        iconName: isRecording ? 'StopRecording' : 'StartRecording',
+        onItemClick: async () => {
             if (!serverCallId) {
                 console.warn('Recording buton: No serverCallId yet!');
                 return;
             }
 
-            if (!!recordingId) {
+            if (isRecording) {
                 // stop the recording
                 await stopRecording({ serverCallId, recordingId });
                 setRecordingId('');
