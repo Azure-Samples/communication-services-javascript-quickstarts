2a25d709ce58e6e5db8a65108905b8d424f6a3f2
Sulochanan <120100226+Sulochanan@users.noreply.github.com>
2023-04-13 03:33:13 +0530

    call-automation-server-recording javascript sample (#1)
    * serverRecording sample with callAutomation SDK
    
    * start and pause recording feature update
    
    * updated API requests
    
    * cleanedup code
    
    * Updated getRecordingFile API
    
    * Updated README file
    
    * formatted files and resolved PR comments
    
    * updated controller file

---
+++ cal-automation-server-recording/Config.ts
@@ -0,0 +1,12 @@
+var config = module.exports = { ConnectionString: '', BlobStorageConnectionString: '', ContainerName: '', CallbackUri: '', StorageAccountName: '', StorageAccountKey: ''};
+
+config = { 'ConnectionString': '', 'BlobStorageConnectionString': '', 'ContainerName': '', 'CallbackUri': '', 'StorageAccountName': '', 'StorageAccountKey': ''};
+
+config.ConnectionString = '';
+config.BlobStorageConnectionString = '';
+config.ContainerName = '';
+config.CallbackUri = '';
+config.StorageAccountName = '';
+config.StorageAccountKey = '';
+
+module.exports = config;
