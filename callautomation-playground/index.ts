f48462ff16dfcf2378d9fc3a9c143749a6a8fb67
ikumarapeli <ikumarapeli@microsoft.com>
2023-05-03 17:06:52 -0700

    update sample

--- callautomation-playground/index.ts
+++ callautomation-playground/index.ts
@@ -2,32 +2,27 @@ import { CallAutomationClient, CallInvite, CallAutomationEvent, parseCallAutomat
 import {CommunicationIdentifier, PhoneNumberIdentifier } from "@azure/communication-common";
 const express = require('express');
 const bodyParser = require ("body-parser")
-
 const app = express();
 app.use(bodyParser.json());
 
 const port = 3000; // default port to listen
 
+//TODO find out why env variable are not working
 const conenctionString = process.env.CONNECTION_STRING || "";
-console.log(conenctionString +"is the cstring")
 const client = new CallAutomationClient(conenctionString)
-
 const eventStore = new Map<String, CallAutomationEvent>();
 
 async function topLevelMenu(callConnection:CallConnection, targetParticipant:CommunicationIdentifier) {
     //TODO move to seperate file
-    //TODO inetraction with dtmf tones
+    //TODO interaction with dtmf tone
 
     const recognizeOptions:CallMediaRecognizeDtmfOptions = {playPrompt: {url:process.env.PLAY_PROMPT||"", kind:"fileSource"},kind:"callMediaRecognizeDtmfOptions"}
-
     const callProperties = await  callConnection.getCallConnectionProperties();
-
     callConnection.getCallMedia().startRecognizing(targetParticipant, 1, recognizeOptions)
 
-    //TODO accept both compelted and failed
-    // should take a type as input
+    //TODO eventAwaiter should accept both compelted and failed events
+    //TODO should take a type as input
     //eventAwaiter(RecognizeCompletedEventData.,callProperties.callConnectionId||"" )
-
 }
 
 function eventAwaiter(eventType:CallAutomationEvent, corelationId:string){
@@ -36,21 +31,15 @@ function eventAwaiter(eventType:CallAutomationEvent, corelationId:string){
 
     for(var time =0; time<60;time++) {
 
-
         if(eventStore.has(eventKey)){
             const event = eventStore.get(eventKey)
             eventStore.delete(eventKey)
             return event
         }
-
         setTimeout(() => {
         }, 1000);
-
     }
-
-    throw console.error("event awaiter tiemout for key"+eventKey);
-    
-
+    throw console.error(`event awaiter tiemout for key: ${eventKey}`);
 } 
 
 // post route
@@ -61,33 +50,26 @@ app.post( "/incomingcall", ( req:any, res:any ) => {
 // post route
 app.post( "/createcall", async ( req:any, res:any ) => {
     console.log("Creating a call")
-    console.log(  req.body)
 
     const calleeNumber = req.body.phoneNumber
-
     const callerNumber = process.env.CALLER_NUMBER || "";
 
     const caller:PhoneNumberIdentifier = {phoneNumber:callerNumber}
     const callee:PhoneNumberIdentifier = {phoneNumber:calleeNumber}
-
     const callInvite:CallInvite = {targetParticipant:callee, sourceCallIdNumber:caller}
-
     const callbackURL = ""+"/events";
 
     const call = await client.createCall(callInvite, callbackURL)
 
-    topLevelMenu(call.callConnection,callee);
-
+    topLevelMenu(call.callConnection, callee);
     res.sendStatus( 200 );
 } );
 
 // post route
 app.post( "/events", ( req:any, res:any ) => {
-
     const event = parseCallAutomationEvent(req.body)
-    eventStore.set(event.kind+":"+event.correlationId, event)
+    eventStore.set(`${event.kind}:${event.correlationId}`, event)
     res.sendStatus( 200 );
-
 } );
 
 // start the Express server
