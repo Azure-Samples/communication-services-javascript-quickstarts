29afb7bd627ff42c3dcb40a6160723e5a62c746c
dmceachernmsft <dmceachern@microsoft.com>
2023-06-23 14:15:56 -0700

    react 18 bump

--- ui-library-click-to-call/src/index.tsx
+++ ui-library-click-to-call/src/index.tsx
@@ -2,11 +2,15 @@
 // Licensed under the MIT license.
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'; // this should point to your App.tsx file
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
