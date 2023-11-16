4fbd7b2eb013c10bedefedfafc4f53e6a4d4741b
williamzhao87 <williamzhao87@gmail.com>
2023-11-16 13:14:48 -0500

    Update quickstart

--- job-router-quickstart/index.js
+++ job-router-quickstart/index.js
@@ -1,57 +1,72 @@
-const { JobRouterClient, JobRouterAdministrationClient } = require('@azure/communication-job-router');
+const createClient = require('@azure-rest/communication-job-router').default;
 
 const main = async () => {
   console.log("Azure Communication Services - Job Router Quickstart")
 
-  // create JobRouterAdministrationClient and JobRouterClient
   const connectionString = process.env["COMMUNICATION_CONNECTION_STRING"] ||
     "endpoint=https://<resource-name>.communication.azure.com/;<access-key>";
-  const routerAdminClient = new JobRouterAdministrationClient(connectionString);
-  const routerClient = new JobRouterClient(connectionString);
-
-  const distributionPolicy = await routerAdminClient.createDistributionPolicy("distribution-policy-1", {
-    offerExpiresAfterSeconds: 60,
-    mode: { kind: "longest-idle" },
-    name: "My distribution policy"
+  const client = createClient(connectionString);
+  
+  const distributionPolicy = await client.path("/routing/distributionPolicies/{distributionPolicyId}", "distribution-policy-1").patch({
+    body: {
+      offerExpiresAfterSeconds: 60,
+      mode: { kind: "longest-idle" },
+      name: "My distribution policy"
+    },
+    contentType: "application/merge-patch+json"
   });
 
-  const queue = await routerAdminClient.createQueue("queue-1", {
-    name: "My Queue",
-    distributionPolicyId: distributionPolicy.id
+  const queue = await client.path("/routing/queues/{queueId}", "queue-1").patch({
+    body: {
+      name: "My Queue",
+      distributionPolicyId: distributionPolicy.body.id
+    },
+    contentType: "application/merge-patch+json"
   });
 
-  const job = await routerClient.createJob("job-1", {
-    channelId: "voice",
-    queueId: queue.id,
-    priority: 1,
-    requestedWorkerSelectors: [{ key: "Some-Skill", labelOperator: "greaterThan", value: 10 }]
+  const job = await client.path("/routing/jobs/{jobId}", "job-1").patch({
+    body: {
+      channelId: "voice",
+      queueId: queue.body.id,
+      priority: 1,
+      requestedWorkerSelectors: [{ key: "Some-Skill", labelOperator: "greaterThan", value: 10 }]
+    },
+    contentType: "application/merge-patch+json"
   });
 
-  let worker = await routerClient.createWorker("worker-1", {
-    totalCapacity: 1,
-    queueAssignments: { [queue.id]: {} },
-    labels: { "Some-Skill": 11 },
-    channelConfigurations: { "voice": { capacityCostPerJob: 1 } },
-    availableForOffers: true
+  let worker = await client.path("/routing/workers/{workerId}", "worker-1").patch({
+    body:  {
+        capacity: 1,
+        queues: [queue.body.id],
+        labels: { "Some-Skill": 11 },
+        channels: [ { channelId: "voice", capacityCostPerJob: 1 } ],
+        availableForOffers: true
+    },
+    contentType: "application/merge-patch+json"
   });
 
   await new Promise(r => setTimeout(r, 10000));
-  worker = await routerClient.getWorker(worker.id);
-  for (const offer of worker.offers) {
-      console.log(`Worker ${worker.id} has an active offer for job ${offer.jobId}`);
+  worker = await client.path("/routing/workers/{workerId}", worker.body.id).get();
+  for (const offer of worker.body.offers) {
+      console.log(`Worker ${worker.body.id} has an active offer for job ${offer.jobId}`);
   }
 
-  const accept = await routerClient.acceptJobOffer(worker.id, worker.offers[0].offerId);
-  console.log(`Worker ${worker.id} is assigned job ${accept.jobId}`);
+  const accept = await client.path("/routing/workers/{workerId}/offers/{offerId}:accept",
+      worker.body.id, worker.body.offers[0].offerId).post();
+  console.log(`Worker ${worker.body.id} is assigned job ${accept.body.jobId}`);
 
-  await routerClient.completeJob(accept.jobId, accept.assignmentId);
-  console.log(`Worker ${worker.id} has completed job ${accept.jobId}`);
+  await client.path("/routing/jobs/{jobId}:complete", accept.body.jobId).post({
+    body: { assignmentId: accept.body.assignmentId }
+  });
+  console.log(`Worker ${worker.body.id} has completed job ${accept.body.jobId}`);
 
-  await routerClient.closeJob(accept.jobId, accept.assignmentId, { dispositionCode: "Resolved" });
-  console.log(`Worker ${worker.id} has closed job ${accept.jobId}`);
+  await client.path("/routing/jobs/{jobId}:complete", accept.body.jobId).post({
+    body: { assignmentId: accept.assignmentId, dispositionCode: "Resolved" }
+  });
+  console.log(`Worker ${worker.body.id} has closed job ${accept.body.jobId}`);
 
-  await routerClient.deleteJob(accept.jobId);
-  console.log(`Deleting job ${accept.jobId}`);
+  await client.path("/routing/jobs/{jobId}", accept.body.jobId).delete();
+  console.log(`Deleting job ${accept.body.jobId}`);
 };
 
 main().catch((error) => {
