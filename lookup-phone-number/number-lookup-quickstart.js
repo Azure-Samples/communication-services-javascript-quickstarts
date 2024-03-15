9668d2ccd43ef2c8b31d042620a6f0a42c9bdf73
Erica <34174887+ericasp16@users.noreply.github.com>
2024-03-14 23:53:12 -0700

    add number lookup sample

---
+++ lookup-phone-number/number-lookup-quickstart.js
@@ -0,0 +1,24 @@
+async function main() {
+    const { PhoneNumbersClient } = require('@azure/communication-phone-numbers');
+
+    // This code retrieves your connection string from an environment variable
+    const connectionString = process.env['COMMUNICATION_SERVICES_CONNECTION_STRING'];
+
+    // Instantiate the phone numbers client
+    const phoneNumbersClient = new PhoneNumbersClient(connectionString);
+
+    // Use the free number lookup functionality to get number formatting information
+    let formattingResults = await phoneNumbersClient.searchOperatorInformation([ "+13154805277" ]);
+    let formatInfo = formattingResults.values[0];
+    console.log(formatInfo.phoneNumber + " is formatted " + formatInfo.internationalFormat + " internationally, and "
+        + formatInfo.nationalFormat + " nationally");
+
+    // Use the paid number lookup functionality to get operator specific details
+    // IMPORTANT NOTE: Invoking the method below will incur a charge to your account
+    let searchResults = await phoneNumbersClient.searchOperatorInformation([ "+13154805277" ], { "includeAdditionalOperatorDetails": true });
+    let operatorInfo = searchResults.values[0];
+    console.log(operatorInfo.phoneNumber + " is a " + (operatorInfo.numberType ? operatorInfo.numberType : "unknown") + " number, operated in "
+        + operatorInfo.isoCountryCode + " by " + (operatorInfo.operatorDetails.name ? operatorInfo.operatorDetails.name : "an unknown operator"));
+}
+
+main();
