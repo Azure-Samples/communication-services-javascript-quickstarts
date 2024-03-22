dbdc510685ec0c68c794a218fa7013f0523280d3
Erica <34174887+ericasp16@users.noreply.github.com>
2024-03-22 11:11:16 -0700

    Update number-lookup-quickstart.js
    update to pull phone number from the command line

--- lookup-phone-number/number-lookup-quickstart.js
+++ lookup-phone-number/number-lookup-quickstart.js
@@ -1,5 +1,12 @@
-async function main() {
+async function main( ) {
     const { PhoneNumbersClient } = require('@azure/communication-phone-numbers');
+	
+	if (process.argv.length < 3)
+	{
+		console.error("missing phone number argument");
+		return;
+	}
+	const phoneNumber = process.argv[2];
 
     // This code retrieves your connection string from an environment variable
     const connectionString = process.env['COMMUNICATION_SERVICES_CONNECTION_STRING'];
@@ -8,14 +15,14 @@ async function main() {
     const phoneNumbersClient = new PhoneNumbersClient(connectionString);
 
     // Use the free number lookup functionality to get number formatting information
-    let formattingResults = await phoneNumbersClient.searchOperatorInformation([ "<target-phone-number>" ]);
+    let formattingResults = await phoneNumbersClient.searchOperatorInformation([ phoneNumber ]);
     let formatInfo = formattingResults.values[0];
     console.log(formatInfo.phoneNumber + " is formatted " + formatInfo.internationalFormat + " internationally, and "
         + formatInfo.nationalFormat + " nationally");
 
     // Use the paid number lookup functionality to get operator specific details
     // IMPORTANT NOTE: Invoking the method below will incur a charge to your account
-    let searchResults = await phoneNumbersClient.searchOperatorInformation([ "<target-phone-number>" ], { "includeAdditionalOperatorDetails": true });
+    let searchResults = await phoneNumbersClient.searchOperatorInformation([ phoneNumber ], { "includeAdditionalOperatorDetails": true });
     let operatorInfo = searchResults.values[0];
     console.log(operatorInfo.phoneNumber + " is a " + (operatorInfo.numberType ? operatorInfo.numberType : "unknown") + " number, operated in "
         + operatorInfo.isoCountryCode + " by " + (operatorInfo.operatorDetails.name ? operatorInfo.operatorDetails.name : "an unknown operator"));
