ec39fa022cab23137927321c4eadd4e4311b1a80
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2024-06-05 15:25:27 -0700

    Updated @azure/communication-react to 1.17.0-beta.1 (#255)

--- ui-library-filesharing-chat-composite/app/src/AttachmentDownloadOptions.tsx
+++ ui-library-filesharing-chat-composite/app/src/AttachmentDownloadOptions.tsx
@@ -1,4 +1,10 @@
-import { AttachmentDownloadOptions, AttachmentMetadata, ChatMessage, defaultAttachmentMenuAction } from "@azure/communication-react";
+import {
+  AttachmentDownloadOptions,
+  AttachmentMenuAction,
+  AttachmentMetadata,
+  ChatMessage,
+  defaultAttachmentMenuAction,
+} from "@azure/communication-react";
 
 const handler = () => {
   // here we are returning a static action for all attachments and all messages
@@ -6,38 +12,47 @@ const handler = () => {
 };
 
 // alternatively, you can return different actions based on the attachment or message
-export const customHandler = (attachment: AttachmentMetadata, message?: ChatMessage) => {
-  if (attachment.extension === "pdf") {
+export const customHandler = (
+  attachment: AttachmentMetadata,
+  message?: ChatMessage
+): AttachmentMenuAction[] => {
+  const extension = attachment.name.split(".").pop() || "";
+  if (extension === "pdf") {
     return [
       {
-        title: "Custom button",
-        icon: (<i className="custom-icon"></i>),
+        name: "Custom button",
+        icon: <i className="custom-icon"></i>,
         onClick: () => {
           window.alert("Custom button clicked");
-        }
+          return Promise.resolve();
+        },
       },
-      defaultAttachmentMenuAction
+      defaultAttachmentMenuAction,
     ];
   } else if (message?.senderId === "user1") {
     return [
       {
-        title: "Custom button 2",
-        icon: (<i className="custom-icon-2"></i>),
+        name: "Custom button 2",
+        icon: <i className="custom-icon-2"></i>,
         onClick: () => {
           window.alert("Custom button 2 clicked");
-        }
+          return Promise.resolve();
+        },
       },
       // you can also override the default action partially
       {
         ...defaultAttachmentMenuAction,
         onClick: () => {
           window.alert("Default action clicked");
-        }
-      }
+          return Promise.resolve();
+        },
+      },
     ];
+  } else {
+    return [defaultAttachmentMenuAction];
   }
-}
+};
 
 export const downloadOptions: AttachmentDownloadOptions = {
-  actionsForAttachment: handler
-}
+  actionsForAttachment: handler,
+};
