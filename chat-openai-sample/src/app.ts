5af3be37c397708dae3c6fe2727c45d86c778ac6
Jim Chou <77021369+jimchou-dev@users.noreply.github.com>
2024-10-21 12:43:26 -0700

    change tab to spaces

--- chat-openai-sample/src/app.ts
+++ chat-openai-sample/src/app.ts
@@ -20,128 +20,128 @@ const translationSystemPrompt = 'Act like you are a agent specialized in generat
 
 /* Azure Open AI Service */
 async function createOpenAiClient() {
-	const openAiServiceEndpoint = process.env.AZURE_OPENAI_SERVICE_ENDPOINT || "";
-	const openAiKey = process.env.AZURE_OPENAI_SERVICE_KEY || "";
-	openAiClient = new OpenAIClient(
-		openAiServiceEndpoint, 
-		new AzureKeyCredential(openAiKey)
-	);
-	console.log("Initialized Open AI Client.");
+    const openAiServiceEndpoint = process.env.AZURE_OPENAI_SERVICE_ENDPOINT || "";
+    const openAiKey = process.env.AZURE_OPENAI_SERVICE_KEY || "";
+    openAiClient = new OpenAIClient(
+        openAiServiceEndpoint, 
+        new AzureKeyCredential(openAiKey)
+    );
+    console.log("Initialized Open AI Client.");
 }
 
 async function getChatCompletions(systemPrompt: string, userPrompt: string){
-	const deploymentName = process.env.AZURE_OPENAI_DEPLOYMENT_MODEL_NAME;
-	const messages = [
-		{ role: "system", content: systemPrompt },
-		{ role: "user", content: userPrompt },
-	];
+    const deploymentName = process.env.AZURE_OPENAI_DEPLOYMENT_MODEL_NAME;
+    const messages = [
+        { role: "system", content: systemPrompt },
+        { role: "user", content: userPrompt },
+    ];
 
-	const response = await openAiClient.getChatCompletions(deploymentName, messages);
-	const responseContent = response.choices[0].message.content;
-	console.log(responseContent);
-	return responseContent;
+    const response = await openAiClient.getChatCompletions(deploymentName, messages);
+    const responseContent = response.choices[0].message.content;
+    console.log(responseContent);
+    return responseContent;
 }
 
 /* Azure Communication Service */
 async function getRecentMessages(token: string, threadId: string) {
-	const chatClient = new ChatClient(ACS_URL_ENDPOINT, new AzureCommunicationTokenCredential(token));
-	const threadClient = chatClient.getChatThreadClient(threadId);
-	const messagesIterator = threadClient.listMessages({ maxPageSize: numMessagesToSummarize });
-	const messages = [];
-	for await (const message of messagesIterator) {
-		messages.push(message);
-	}
-	return messages;
+    const chatClient = new ChatClient(ACS_URL_ENDPOINT, new AzureCommunicationTokenCredential(token));
+    const threadClient = chatClient.getChatThreadClient(threadId);
+    const messagesIterator = threadClient.listMessages({ maxPageSize: numMessagesToSummarize });
+    const messages = [];
+    for await (const message of messagesIterator) {
+        messages.push(message);
+    }
+    return messages;
 }
 
 async function getMessage(token: string, threadId: string, messageId: string) {
-	const chatClient = new ChatClient(ACS_URL_ENDPOINT, new AzureCommunicationTokenCredential(token));
-	const threadClient = chatClient.getChatThreadClient(threadId);
-	const message = await threadClient.getMessage(messageId);
-	return message;
+    const chatClient = new ChatClient(ACS_URL_ENDPOINT, new AzureCommunicationTokenCredential(token));
+    const threadClient = chatClient.getChatThreadClient(threadId);
+    const message = await threadClient.getMessage(messageId);
+    return message;
 }
 
 /* API routes */
 app.get('/api/chat/:threadId/summary', async (req: any, res: any)=>{
-	// Authnorization header format: "Bearer <ACS_TOKEN>"
-	const token = req.headers['authorization'].split(' ')[1];
-	const { threadId } = req.params;
-	try{
-		const messages = await getRecentMessages(token, threadId);
-		const result = await getChatCompletions(summarizationSystemPrompt, JSON.stringify(messages))
-		res.json(result);
-	}
-	catch(error){
-		console.error("Error during get summary.", error);
-	}
+    // Authnorization header format: "Bearer <ACS_TOKEN>"
+    const token = req.headers['authorization'].split(' ')[1];
+    const { threadId } = req.params;
+    try{
+        const messages = await getRecentMessages(token, threadId);
+        const result = await getChatCompletions(summarizationSystemPrompt, JSON.stringify(messages))
+        res.json(result);
+    }
+    catch(error){
+        console.error("Error during get summary.", error);
+    }
 });
 
 app.get('/api/chat/:threadId/message/:messageId/sentiment', async (req: any, res: any)=>{
-	// Authnorization header format: "Bearer <ACS_TOKEN>"
-	const token = req.headers['authorization'].split(' ')[1];
-	const { threadId, messageId } = req.params;
-	try{
-		const message = await getMessage(token, threadId, messageId);
-		const result = await getChatCompletions(sentimentSystemPrompt, message.content.message);
-		res.json(result);
-	}
-	catch(error){
-		console.error("Error during get sentiment.", error);
-	}
+    // Authnorization header format: "Bearer <ACS_TOKEN>"
+    const token = req.headers['authorization'].split(' ')[1];
+    const { threadId, messageId } = req.params;
+    try{
+        const message = await getMessage(token, threadId, messageId);
+        const result = await getChatCompletions(sentimentSystemPrompt, message.content.message);
+        res.json(result);
+    }
+    catch(error){
+        console.error("Error during get sentiment.", error);
+    }
 });
 
 app.get('/api/chat/:threadId/message/:messageId/translation/:language', async (req: any, res: any)=>{
-	// Authnorization header format: "Bearer <ACS_TOKEN>"
-	const token = req.headers['authorization'].split(' ')[1];
-	const { threadId, messageId, language } = req.params;
-	try{
-		const message = await getMessage(token, threadId, messageId);
-		const systemPrompt = translationSystemPrompt.replace('TARGET_LANGUAGE', language);
-		const result = await getChatCompletions(systemPrompt, message.content.message);
-		res.json(result);
-	}
-	catch(error){
-		console.error("Error during get translation.", error);
-	}
+    // Authnorization header format: "Bearer <ACS_TOKEN>"
+    const token = req.headers['authorization'].split(' ')[1];
+    const { threadId, messageId, language } = req.params;
+    try{
+        const message = await getMessage(token, threadId, messageId);
+        const systemPrompt = translationSystemPrompt.replace('TARGET_LANGUAGE', language);
+        const result = await getChatCompletions(systemPrompt, message.content.message);
+        res.json(result);
+    }
+    catch(error){
+        console.error("Error during get translation.", error);
+    }
 });
 
 // EventGrid
 app.post("/api/chatMessageReceived", async (req: any, res:any)=>{
-	console.log(`Received chatMessageReceived event - data --> ${JSON.stringify(req.body)} `);
-	const event = req.body[0];
+    console.log(`Received chatMessageReceived event - data --> ${JSON.stringify(req.body)} `);
+    const event = req.body[0];
 
-	try{
-		const eventData = event.data;
-		if (event.eventType === "Microsoft.EventGrid.SubscriptionValidationEvent") {
-			console.log("Received SubscriptionValidation event");
-			res.status(200).json({
-				validationResponse: eventData.validationCode
-			});
-			return;
-		}
+    try{
+        const eventData = event.data;
+        if (event.eventType === "Microsoft.EventGrid.SubscriptionValidationEvent") {
+            console.log("Received SubscriptionValidation event");
+            res.status(200).json({
+                validationResponse: eventData.validationCode
+            });
+            return;
+        }
 
-		const messageId = event.data.messageId;
+        const messageId = event.data.messageId;
 
-		// Sentiment Analysis
-		const sentimentResult = await getChatCompletions(sentimentSystemPrompt, event.data.messageBody);
-		console.log(`Sentiment ${messageId}: ${sentimentResult}`);
+        // Sentiment Analysis
+        const sentimentResult = await getChatCompletions(sentimentSystemPrompt, event.data.messageBody);
+        console.log(`Sentiment ${messageId}: ${sentimentResult}`);
 
-		// Translation
-		const translaitonPrompt = translationSystemPrompt.replace('TARGET_LANGUAGE', TRANSLATION_LANGUAGE);
-		const translationResult = await getChatCompletions(translaitonPrompt, event.data.messageBody);
-		console.log(`Translating ${messageId}: ${translationResult}`);
-	}
-	catch(error){
-		console.error("Error during the message recieved event.", error);
-	}
+        // Translation
+        const translaitonPrompt = translationSystemPrompt.replace('TARGET_LANGUAGE', TRANSLATION_LANGUAGE);
+        const translationResult = await getChatCompletions(translaitonPrompt, event.data.messageBody);
+        console.log(`Translating ${messageId}: ${translationResult}`);
+    }
+    catch(error){
+        console.error("Error during the message recieved event.", error);
+    }
 });
 
 app.get('/', (req, res) => {
-	res.send('Hello ACS Chat!');
+    res.send('Hello ACS Chat!');
 });
 
 // Start the server
 app.listen(PORT, async () => {
-	console.log(`Server is listening on port ${PORT}`);
-	await createOpenAiClient();
+    console.log(`Server is listening on port ${PORT}`);
+    await createOpenAiClient();
 });
