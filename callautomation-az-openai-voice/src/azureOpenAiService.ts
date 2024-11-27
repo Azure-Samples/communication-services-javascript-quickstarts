61e895789b032cb4f39af80215239370a8f419e9
Vinothini Dharmaraj <v-vdharmaraj@microsoft.com>
2024-11-27 01:06:06 -0800

    updating the AI Prompt

--- callautomation-az-openai-voice/src/azureOpenAiService.ts
+++ callautomation-az-openai-voice/src/azureOpenAiService.ts
@@ -10,11 +10,7 @@ const openAiServiceEndpoint = process.env.AZURE_OPENAI_SERVICE_ENDPOINT || "";
 const openAiKey = process.env.AZURE_OPENAI_SERVICE_KEY || "";
 const openAiDeploymentModel = process.env.AZURE_OPENAI_DEPLOYMENT_MODEL_NAME || "";
 
-const answerPromptSystemTemplate = `You're an AI assistant for an elevator company called Contoso Elevators. Customers will contact you as the first point of contact when having issues with their elevators. 
-Your priority is to ensure the person contacting you or anyone else in or around the elevator is safe, if not then they should contact their local authorities.
-If everyone is safe then ask the user for information about the elevators location, such as city, building and elevator number.
-Also get the users name and number so that a technician who goes onsite can contact this person. Confirm with the user all the information 
-they've shared that it's all correct and then let them know that you've created a ticket and that a technician should be onsite within the next 24 to 48 hours.`
+const answerPromptSystemTemplate = `You are an AI assistant that helps people find information.`
 
 let realtimeStreaming: LowLevelRTClient;
 
