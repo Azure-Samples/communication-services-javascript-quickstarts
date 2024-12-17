4c74bab7e333f96317046bcc0e4b15981748971b
Amari West <amariwest@microsoft.com>
2024-12-17 10:31:03 -0800

    Sanitize user input to prevent XSS in RecordingsController

--- call-recording/api/controllers/RecordingsController.ts
+++ call-recording/api/controllers/RecordingsController.ts
@@ -10,6 +10,8 @@ import { Request, Response } from "express";
 import { PhoneNumberIdentifier } from "@azure/communication-common";
 import { SubscriptionValidationEventData, AcsRecordingFileStatusUpdatedEventData } from "@azure/eventgrid";
 
+const escapeHtml = require('escape-html');
+
 var cfg = require("../../config");
 var CallAutomationClient = require("@azure/communication-call-automation");
 const { Logger, MessageType } = require("../../Logger");
@@ -117,8 +119,8 @@ exports.recordingFileStatus = async function (req: Request, res: Response) {
 
     if (eventGridEvent.eventType == "Microsoft.Communication.RecordingFileStatusUpdated") {
       var statusUpdated: AcsRecordingFileStatusUpdatedEventData = eventGridEvent.data;
-      contentLocation = statusUpdated.recordingStorageInfo.recordingChunks[0].contentLocation;
-      deleteLocation = statusUpdated.recordingStorageInfo.recordingChunks[0].deleteLocation;
+      contentLocation = escapeHtml(statusUpdated.recordingStorageInfo.recordingChunks[0].contentLocation);
+      deleteLocation = escapeHtml(statusUpdated.recordingStorageInfo.recordingChunks[0].deleteLocation);
     }
   }
   return res.status(200).send(`Recording Download Location :${contentLocation}, Recording Delete Location: ${deleteLocation}`);
