bb7c0a652819b0ae616fbe3c87ce568e0482c3d7
Cezara Zamfir <66410283+cemateia@users.noreply.github.com>
2025-07-11 15:44:12 +0200

    Update config.js

--- tpe-token-and-access-management/client/config.js
+++ tpe-token-and-access-management/client/config.js
@@ -5,12 +5,12 @@
 export const config = {
     // Azure Communication Services resource endpoint
     // Format: https://your-acs-resource.communication.azure.com/
-    acsResourceEndpoint: 'https://ops-demo-test-1.unitedstates.communication.azure.com/',
+    acsResourceEndpoint: '<YOUR-RESOURCE-ENDPOINT>',
     
     // Azure AD application configuration
     azureAD: {
-        clientId: '7df5ab42-bf69-40f1-8734-41f010af88bb',
-        tenantId: 'be5c2424-1562-4d62-8d98-815720d06e4a'
+        clientId: '<ENTRA-CLIENT-ID>',
+        tenantId: '<ENTRA-TENANT-ID>'
     },
     
     // Azure Communication Services scopes
